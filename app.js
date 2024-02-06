5a53f76e0ee44e51c3c248b20ec9971bf10a0efc
Volodymyr_D <dymyr307@gmail.com>
2024-02-06 23:07:52 +0200

    modified js

--- app.js
+++ app.js
@@ -2,3 +2,29 @@ const btn = document.getElementById('myBtn');
 const modal = document.getElementById('myModal');
 
 btn.addEventListener('click', openModal);
+
+function openModal() {
+  modal.classList.add('open');
+  attachModalEvents();
+}
+
+function attachModalEvents() {
+  modal.querySelector('.close').addEventListener('click', closeModal);
+  document.addEventListener('keydown', handleEscape);
+}
+
+function handleEscape(event) {
+  console.log(event.key);
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+}
+
+function closeModal() {
+  modal.classList.remove('open');
+  detachModalEvents();
+}
+function detachModalEvents() {
+  modal.querySelector('.close').removeEventListener('click', closeModal);
+  document.removeEventListener('keydown', handleEscape);
+}
